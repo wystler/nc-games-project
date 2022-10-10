8f9dad4a8490b4731ea80924c2e7989787bfd425
wystler <benjamin.rudd@live.com>
2022-10-10 14:23:03 +0100

    test for /api/categories to return all the category objects and error testing forinvalid path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,4 @@
+const { TestWatcher } = require('jest')
 const request = require('supertest')
 const app = require('../app.js')
 
@@ -21,6 +22,63 @@ afterAll(() => {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-describe('api tests', () => {
-    test('')
+describe('API tests', () => {
+  describe('Endpoint testing', () => {
+
+    test('return an array of objects with slug and description properties', () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({body}) => {
+          expect(body).toBeInstanceOf(Array)
+          expect(body).toHaveLength(8)
+          expect(body.hasOwnProperty("slug")).toBe(true)
+          expect(body.hasOwnProperty("description")).toBe(true)
+        })
+      })
+  })
+
+  describe('Error handling tests', () => {
+
+    test('return "status:404, Route not found', () => {
+      return request(app)
+        .get('/api/someRandomThingThatIsntAValidPath')
+        .expect(404)
+        .then(({body}) => {
+          expect(body.msg).toBe("Route not found")
+        })
+    })
+  })
 })
+
+
+
+//   test("status:200, responds with an array of treasure objects", () => {
+//     //  invoke it with the end point being tested
+// return request(app)
+//   .get("/api/")
+//     //  hopefully get a response code
+//   .expect(200)
+//     //  and some stuff in the body
+//   .then(({ body }) => {
+//     const { treasures } = body;
+//     //  that should be an array in this case
+//     expect(treasures).toBeInstanceOf(Array);
+//     //  with 26 objects inside
+//     expect(treasures).toHaveLength(26);
+//     treasures.forEach((treasure) => {
+//       expect(treasure).toEqual(
+//     //  with these characteristics
+//         expect.objectContaining({
+//           treasure_id: expect.any(Number),
+//           treasure_name: expect.any(String),
+//           colour: expect.any(String),
+//           age: expect.any(Number),
+//           cost_at_auction: expect.any(Number),
+//           shop_id: expect.any(Number),
+//         })
+//       );
+//     });
+//   });
+// });
+// })
